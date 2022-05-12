f448d63e2260770efc5a13d85c18a02677fde904
JirkaMedek <jirka.medek95@gmail.com>
2022-05-12 21:28:28 +0200

    Updates

---
+++ precache-manifest.406df5a5acfc1ec711ba4bc7f90c63a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c7d55b03e9996d7bedc",
+    "url": "/my-app/static/css/main.5bf01702.chunk.css"
+  },
+  {
+    "revision": "3c7d55b03e9996d7bedc",
+    "url": "/my-app/static/js/main.3c7d55b0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/my-app/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3ed16eb1a533e87cf871255e69aa3782",
+    "url": "/my-app/index.html"
+  }
+];
